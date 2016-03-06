1b387430a13bb81de2e3f663f5bcb90aacb1ecd0
Brian Bahmanyar <abahmanyar1995@gmail.com>
2016-03-06 09:39:30 -0800

    cleaning and adding markup

--- helpers.py
+++ helpers.py
@@ -2,26 +2,53 @@ import numpy as np
 import pandas as pd
 import Quandl
 
-
-def get_adj_close(tickers, start="", end="", ratio=True):
+def get_adj_close(tickers, start, end="", ratios=[], log_transforms=[]):
     """
-    Returns DataFrame with the adjusted close values for the 
-        tickers passed in during the time frame specified
-    """
+    Args:
+        tickers (list): collection of ticker symbols for which to collect adj. close 
+                daily prices for
+        start (string, format: 2013-01-01): start date for which to collect prices after
+        end (string, format: 2013-01-01): optional end date, today if not specified
+        ratios (list): collection of tuples of tickers from 'tickers' list to calculate 
+                price ratios for (the stock with larger mean is numerator)
+        log_transforms (list): collection of tickers from 'tickers' to include additional 
+                natural log transformed copies
     
+    Returns (dataframe): all adj. close prices, ratios, and log transforms specified
+    """ 
     result = {}
     
     for ticker in tickers:
         try:
-            result[ticker] = Quandl.get("WIKI/"+ticker, trim_start=start, trim_end=end)['Adj. Close']
+            result[ticker] = Quandl.get('WIKI/'+ticker, trim_start=start, trim_end=end)['Adj. Close']
         except DatasetNotFound:
-            print(ticker, 'is not a vaild ticker')
-            return
+            print('ERROR:', ticker, 'is not a vaild ticker')
+        except ErrorDownloading:
+            print('ERROR: API Limit reached')
+
+    for ratio in ratios:
+        try:
+            ticker1, ticker2 = ratio
+            if result[ticker1].mean() > result[ticker2].mean():
+                result[ticker1+'/'+ticker2] = result[ticker1]/result[ticker2]
+            else:
+                result[ticker2+'/'+ticker1] = result[ticker2]/result[ticker1]
+        except KeyError:
+            print('ERROR:', ticker1, 'or', ticker2, 'are not in the list of specified tickers')
     
-    if ratio:
-        if result[tickers[0]].mean() > result[tickers[1]].mean():
-            result[tickers[0]+'/'+tickers[1]] = result[tickers[0]]/result[tickers[1]]
-        else:
-            result[tickers[1]+'/'+tickers[0]] = result[tickers[1]]/result[tickers[0]]
+    for log_transform in log_transforms:
+        try:
+            result['ln('+log_transform+')'] = np.log(result[log_transform])
+        except KeyError:
+            print('ERROR:', log_transform, 'is not in the list of specified tickers')
     
-    return pd.DataFrame(result).dropna()
+    return pd.DataFrame(result).dropna() # drop na here because of differences in lenght of history for stocks
+
+def get_lagged_series(series, order=1, log=False):
+    lagged = None
+    if log:
+        lagged = np.log(series).diff(order).dropna();
+    else:
+        lagged = series.diff(order).dropna();
+    
+    return lagged
---
+++ plots.py
@@ -0,0 +1,97 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.dates import *
+import seaborn as sns
+import statsmodels.api as sm
+from helpers import get_lagged_series
+
+sns.set_style("whitegrid", {'axes.edgecolor': '.6', 
+                            'axes.facecolor': '0.9', 
+                            'grid.color': '.82', 
+                            'legend.frameon': True,
+                            'axes.labelsize': 'small'})
+
+def plot_stocks(index, stocks, labels, positions=None, label_annually=True):
+    """
+    Plots up to 5 stocks
+    
+    Args:
+        index (DateTimeIndex): date range
+        stocks (list): list of stocks to plot
+        labels (list): labels to use for plotting
+        positions (list of dicts): optional postions to overplot
+        label_annually (boolean): plots x label monthly if false, annually otherwise
+        
+    Returns (None): Will output plot inline
+    """
+    colors = ['firebrick','steelblue','orange','mediumorchid', 'mediumseagreen']
+    
+    fig, ax = plt.subplots(figsize=(20,8));
+    
+    if label_annually:
+        span = YearLocator();
+        my_format = DateFormatter('%Y');
+    else:
+        span = MonthLocator();
+        my_format = DateFormatter('%b %Y');    
+    
+    ax.xaxis.set_major_locator(span);
+    ax.xaxis.set_major_formatter(my_format);
+    ax.autoscale_view();
+    
+    plt.title('Adjusted Close Prices', fontsize=20);
+    plt.ylabel('Adj. Close', fontsize=15);
+    plt.xlabel('Time', fontsize=15);
+    
+    for stock, label, color in zip(stocks, labels, colors):
+        ax.plot_date(index, stock, color=color, linestyle='-', marker=None, label=label);
+        plt.xticks(rotation=45)
+    
+    if positions:
+        for position in positions:
+            ax.axvline(df.index[position['open']], color='lime', linestyle='-')
+            ax.axvline(df.index[position['close']], color='red', linestyle='-')
+ 
+    plt.legend(loc=2, prop={'size':15}, frameon=True);
+
+def plot_lagged_series(series, order=1, log=False):
+    """
+    Plots lagged series
+    
+    Args:
+        series (ndarray): time series
+        order (int): the order of the difference
+        log (boolean): whether to log transform before difference
+    
+    Returns (None): plots inline
+    """
+    plt.figure(figsize=(8,4));
+    plt.xlabel('Time Index');
+    plt.ylabel('Difference');
+    
+    if log:
+        plt.title('Log Series Lagged by ' + str(order));
+        lagged = get_lagged_series(series, order, log)
+    else:
+        plt.title('Series Lagged by ' + str(order));
+        lagged = get_lagged_series(series, order, log)
+        
+    plt.plot(np.arange(0,lagged.size,1), lagged);
+
+def plot_correlograms(series, limit=50):
+    fig = plt.figure(figsize=(15,8));
+    ax1 = fig.add_subplot(211);
+    fig = sm.graphics.tsa.plot_acf(series, lags=limit, ax=ax1);
+    plt.title('Correlogram');
+    plt.xticks(np.arange(0,limit+1,1))
+    plt.xlim([-1,limit])
+    
+    ax2 = fig.add_subplot(212);
+    fig = sm.graphics.tsa.plot_pacf(series, lags=limit, ax=ax2);
+    plt.title('Partial Correlogram');
+    plt.xticks(np.arange(0,limit+1,1))
+    plt.xlim([-1,limit])
+
+
+
